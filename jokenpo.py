67e99cd961afa2e342774ab061c09a2504df2a09
Ryan <negoryanaraujo@gmail.com>
2023-03-27 23:07:57 -0300

    colocando sleep

--- jokenpo.py
+++ jokenpo.py
@@ -1,4 +1,5 @@
 from random import randint
+from time import sleep
 itens = ('Pedra', 'Papel', 'Tesoura')
 computador = randint(0, 2)
 print('''Suas opções:
@@ -6,6 +7,11 @@ print('''Suas opções:
 [1] Papel
 [2] Tesoura''')
 jogador = int(input('Qual é a sua jogada? '))
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PO!')
 print('~~' * 12)
 print(f'O computador jogou {itens[computador]}')
 print(f'O jogador jogou {itens[jogador]}')
